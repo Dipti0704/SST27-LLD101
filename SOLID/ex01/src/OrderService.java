e0ee515abc866b62981e679639e4cfb36fb23613
Dipti0704 <dipti820h@gmail.com>
2025-08-25 01:35:32 +0530

    refactored ex01, ex03, ex04

--- SOLID/ex01/src/OrderService.java
+++ SOLID/ex01/src/OrderService.java
@@ -14,3 +14,7 @@ public class OrderService {
         System.out.println("Order stored (pretend DB).");
     }
 }
+
+// what i am thinking is, instead of keeping totalwithtax nd sending email in orderservice we have to keep it seperately... either by creating interface or something else and then after doing the tax collection there then we can use that total amount....
+// send email is also in orderservice... so its voilating the DIP rule.. dependency inversion principle... since this is the example of tighltly coupling .... why are we using orderservice to send emails.... that why it should be depended on abstractions.
+
